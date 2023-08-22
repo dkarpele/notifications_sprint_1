3db74413db9559bc806e2b2ca8dab89397a0f434
Denis Karpelevich <dkarpele@redhat.com>
2023-08-22 23:19:59 +0200

    fix linters

--- notifications_api/receive.py
+++ notifications_api/receive.py
@@ -4,7 +4,8 @@ import sys
 
 
 def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
     channel = connection.channel()
 
     channel.queue_declare(queue='hello')
@@ -12,7 +13,8 @@ def main():
     def callback(ch, method, properties, body):
         print(f" [x] Received {body}")
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=callback,
+                          auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
