9de08a124c5c22d1cbc61a43124d75798f49c4ee
lubovSovina <sovinaliubov@yandex.ru>
2023-09-03 16:30:25 +0300

    Add functions to send messages

--- notifications/src/smtp/send_emails.py
+++ notifications/src/smtp/send_emails.py
@@ -1,20 +1,62 @@
 import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
+from jinja2 import Environment, FileSystemLoader
 from dotenv import load_dotenv
 
-
 load_dotenv()
-message = Mail(
-    from_email=f'{os.environ.get("FROM_EMAIL")}',
-    to_emails=f'{os.environ.get("FROM_EMAIL")}',
-    subject='Sending with Twilio SendGrid is Fun',
-    html_content='<strong>and easy to do anywhere, even with Python</strong>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e)
+
+
+def send_email_registered(data: dict):
+    current_path = os.path.dirname(__file__)
+    loader = FileSystemLoader(current_path)
+    env = Environment(loader=loader)
+    template = env.get_template('registered.html')
+    template_data = {
+        "first_name": data['first_name'],
+        "last_name": data['last_name']
+    }
+    output = template.render(**template_data)
+
+    message = Mail(
+        from_email=f'{os.environ.get("FROM_EMAIL")}',
+        to_emails=data['user_email'],
+        subject='User registration confirmation',
+        html_content=output)
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
+
+
+def send_email_likes(data: dict):
+    current_path = os.path.dirname(__file__)
+    loader = FileSystemLoader(current_path)
+    env = Environment(loader=loader)
+    template = env.get_template('likes_for_review.html')
+    for user_id in data:
+        to_email = data[user_id][0]
+        template_data = {
+            "first_name": data[user_id][1],
+            "last_name": data[user_id][2],
+            "reviews": data[user_id][3:]
+        }
+        output = template.render(**template_data)
+
+        message = Mail(
+            from_email=f'{os.environ.get("FROM_EMAIL")}',
+            to_emails=to_email,
+            subject='Your best comments today! ',
+            html_content=output)
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
