6fc0a73ca5fbd03464a15a8743fb646bbf0c538f
lubovSovina <sovinaliubov@yandex.ru>
2023-09-07 12:03:36 +0300

    Add this new function to the end of  send_registered and send_likes

--- notifications/src/message_worker/send_emails.py
+++ notifications/src/message_worker/send_emails.py
@@ -39,6 +39,7 @@ class Email(AbstractMessage):
             logging.info(f'Sendgrid message body: {response.body}')
             logging.info(f'Sendgrid headers:\n {response.headers}')
             await self.change_db_status(correlation_id)
+            await self.add_notifications_history(data['id'], data['email'], template_data)
         except Exception as e:
             logging.error(e)
 
@@ -70,5 +71,6 @@ class Email(AbstractMessage):
                 logging.info(f'Sendgrid message body: {response.body}')
                 logging.info(f'Sendgrid headers:\n {response.headers}')
                 await self.change_db_status(correlation_id)
+                await self.add_notifications_history(user_id, to_email, template_data)
             except Exception as e:
                 logging.error(e)
